32729e9192b68acd5f9f3150e6aa0692eb30e311
sjfeng1999 <j514681085@icloud.com>
2022-05-16 18:51:31 +0800

    [iree] add tnn module

---
+++ runtime/tnn_iree_runtime.cpp
@@ -0,0 +1,139 @@
+//
+//
+//
+//
+
+#include "iree/base/internal/file_io.h"
+#include "iree/runtime/api.h"
+#include "iree/hal/vmvx/registration/driver_module.h"
+#include "iree/modules/hal/module.h"
+#include "iree/vm/bytecode_module.h"
+#include "iree/vm/ref_cc.h"
+#include "iree/tools/utils/image_util.h"
+
+#include "iree/testing/gtest.h"
+#include "iree/testing/status_matchers.h"
+
+#include "runtime/tnn_module.h"
+
+#include <stdio.h>
+#include <type_traits>
+#include <vector>
+
+iree_vm_function_t LookupFunction(const char* function_name, iree_vm_module_t* bytecode_module) {
+    iree_vm_function_t function;
+    IREE_CHECK_OK(bytecode_module->lookup_function(
+        bytecode_module->self, IREE_VM_FUNCTION_LINKAGE_EXPORT,
+        iree_make_cstring_view(function_name), &function));
+    return function;
+}
+
+
+int main(int argc, char* argv[]){
+
+    iree_vm_instance_t* instance_ = nullptr;
+    iree_vm_context_t* context_ = nullptr;
+    iree_vm_module_t* bytecode_module_ = nullptr;
+    iree_vm_module_t* native_module_ = nullptr;
+    iree_vm_module_t* hal_module_ = nullptr;
+    iree_hal_allocator_t* hal_allocator_ = nullptr;
+
+    iree_hal_driver_t* hal_driver = nullptr;
+    iree_hal_device_t* hal_device = nullptr;
+
+    IREE_CHECK_OK(iree_hal_vmvx_driver_module_register(iree_hal_driver_registry_default()));
+
+    IREE_CHECK_OK(iree_vm_instance_create(iree_allocator_system(), &instance_));
+
+
+    IREE_CHECK_OK(iree_hal_module_register_types());
+
+    IREE_CHECK_OK(iree_hal_driver_registry_try_create_by_name(
+        iree_hal_driver_registry_default(), iree_make_cstring_view("vmvx"),
+        iree_allocator_system(), &hal_driver));
+
+    IREE_CHECK_OK(iree_hal_driver_create_default_device(
+        hal_driver, iree_allocator_system(), &hal_device));
+
+    IREE_CHECK_OK(iree_hal_module_create(hal_device, iree_allocator_system(),
+                                         &hal_module_));
+    hal_allocator_ = iree_hal_device_allocator(hal_device);
+    
+
+    IREE_CHECK_OK(iree_tnn_module_register_types());
+    IREE_CHECK_OK(iree_tnn_module_create(iree_allocator_system(), hal_allocator_, &native_module_));
+
+
+    iree_file_contents_t* flatbuffer_contents = NULL;
+    const char* module_file = {"../model/tnn_iree_mnist.vmfb"};
+
+    printf("IREE module loaded by vmfb file\n");
+
+    IREE_CHECK_OK(iree_file_read_contents(module_file, iree_allocator_system(), &flatbuffer_contents));
+
+    IREE_CHECK_OK(iree_vm_bytecode_module_create(
+        flatbuffer_contents->const_buffer,
+        iree_file_contents_deallocator(flatbuffer_contents),
+        iree_allocator_system(), &bytecode_module_));
+
+
+    std::vector<iree_vm_module_t*> modules = {hal_module_, native_module_, bytecode_module_};
+
+    IREE_CHECK_OK(iree_vm_context_create_with_modules(
+        instance_, IREE_VM_CONTEXT_FLAG_NONE, modules.data(), modules.size(),
+        iree_allocator_system(), &context_));
+
+
+    iree_string_view_t image_path = iree_make_cstring_view("../model/mnist_test.png");
+    iree_hal_buffer_view_t* buffer_view = NULL;
+    iree_hal_dim_t buffer_shape[] = {1, 28, 28, 1};
+    iree_hal_element_type_t hal_element_type = IREE_HAL_ELEMENT_TYPE_FLOAT_32;
+    float input_range[2] = {0.0f, 1.0f};
+  
+    IREE_CHECK_OK(
+        iree_tools_utils_buffer_view_from_image_rescaled(
+            image_path, buffer_shape, IREE_ARRAYSIZE(buffer_shape),
+            hal_element_type, iree_hal_device_allocator(hal_device), input_range,
+            IREE_ARRAYSIZE(input_range), &buffer_view));
+
+
+    iree::vm::ref<iree_vm_list_t> inputs;
+    IREE_CHECK_OK(iree_vm_list_create(/*element_type=*/nullptr, 1,
+                                       iree_allocator_system(), &inputs));
+
+
+    iree_vm_ref_t input_buffer_ref = iree_hal_buffer_view_move_ref(buffer_view);
+    IREE_CHECK_OK(iree_vm_list_push_ref_move(inputs.get(), &input_buffer_ref));
+
+
+    iree::vm::ref<iree_vm_list_t> outputs;
+    IREE_CHECK_OK(iree_vm_list_create(/*element_type=*/nullptr, 1,
+                                       iree_allocator_system(), &outputs));
+
+
+
+    IREE_CHECK_OK(iree_vm_invoke(context_, LookupFunction("predict", bytecode_module_),
+                                  IREE_VM_INVOCATION_FLAG_NONE,
+                                  /*policy=*/nullptr, inputs.get(), outputs.get(),
+                                  iree_allocator_system()));
+
+
+    iree_custom_message_t* reversed_message =
+        (iree_custom_message_t*)iree_vm_list_get_ref_deref(
+            outputs.get(), 0, iree_custom_message_get_descriptor());
+
+
+    char result_buffer[256];
+    IREE_CHECK_OK(iree_custom_message_read_value(reversed_message, result_buffer,
+                                                  IREE_ARRAYSIZE(result_buffer)));
+
+
+    iree_hal_device_release(hal_device);
+    iree_hal_driver_release(hal_driver);
+    // iree_vm_module_release(hal_module_);
+    // iree_vm_module_release(native_module_);
+    // iree_vm_module_release(bytecode_module_);
+    iree_vm_context_release(context_);
+    iree_vm_instance_release(instance_);
+    return 0;
+}
---
+++ runtime/tnn_module.h
@@ -0,0 +1,57 @@
+//
+//
+//
+//
+
+#ifndef IREE_SAMPLES_CUSTOM_MODULES_NATIVE_MODULE_H_
+#define IREE_SAMPLES_CUSTOM_MODULES_NATIVE_MODULE_H_
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include "iree/base/api.h"
+#include "iree/hal/api.h"
+#include "iree/vm/api.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif  // __cplusplus
+
+typedef struct iree_custom_message_t iree_custom_message_t;
+typedef struct iree_tnn_blob_t iree_tnn_blob_t;
+
+// Creates a new !custom.message object with a copy of the given |value|.
+iree_status_t iree_custom_message_create(iree_string_view_t value,
+                                         iree_allocator_t allocator,
+                                         iree_custom_message_t** out_message);
+
+// Wraps an externally-owned |value| in a !custom.message object.
+iree_status_t iree_custom_message_wrap(iree_string_view_t value,
+                                       iree_allocator_t allocator,
+                                       iree_custom_message_t** out_message);
+
+// Copies the value of the !custom.message to the given output buffer and adds
+// a \0 terminator.
+iree_status_t iree_custom_message_read_value(iree_custom_message_t* message,
+                                             char* buffer,
+                                             size_t buffer_capacity);
+
+// Registers the custom types used by the module.
+// WARNING: not thread-safe; call at startup before using.
+iree_status_t iree_tnn_module_register_types(void);
+
+// Creates a native custom module.
+// Modules may exist in multiple contexts and should be thread-safe and (mostly)
+// immutable. Use the per-context allocated state for retaining data.
+iree_status_t iree_tnn_module_create(
+    iree_allocator_t host_allocator, iree_hal_allocator_t* device_allocator,
+    iree_vm_module_t** out_module);
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif  // __cplusplus
+
+IREE_VM_DECLARE_TYPE_ADAPTERS(iree_custom_message, iree_custom_message_t);
+IREE_VM_DECLARE_TYPE_ADAPTERS(iree_tnn_blob, iree_tnn_blob_t);
+
+#endif  // IREE_SAMPLES_CUSTOM_MODULES_NATIVE_MODULE_H_
